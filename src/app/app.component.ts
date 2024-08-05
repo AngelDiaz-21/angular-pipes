4442c1ba2e3db9f7212938b8175faac3f4f5d74d
Angel <angelsistemas21@gmail.com>
2024-08-05 17:57:36 -0600

    feat: PrimeNg dependency injection and implement rippple effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PrimeNGConfig} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipesApp';
+// Ripple es una animacion visual que se activa cuando el usuario hace clic en un elemento de la interfaz. Ripple effect (efecto de ondulación). Al agregarlo aquí se habilita el efecto globalmente en todos los componentes de PrimeNG que soportan esta característica
+  constructor(private primengConfig: PrimeNGConfig){}
+
+  ngOnInit(){
+    this.primengConfig.ripple = true;
+  }
+
+  public title = 'pipesApp';
 }
