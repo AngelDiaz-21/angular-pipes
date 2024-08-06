f2f048ec832e14499c2103e4e1657534341e1a35
Angel <angelsistemas21@gmail.com>
2024-08-06 11:29:42 -0600

    feat: Configure registerLocaleData, import SharedModule and BrowserAnimationsModule, set app language to es-MX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,36 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+// Configuracion del locale de la app(esto es para el pipe de la fecha), se selecciona el idioma de mexico
+import localEsMX from '@angular/common/locales/es-MX';
+import localFrCA from '@angular/common/locales/fr-CA'; //Este es frances de canada
+
+// Se tiene que llamar para que angular establezca el idioma o idiomas(ya que se puede usar mas de uno) que se deseen maneja en la app
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEsMX);
+registerLocaleData(localFrCA);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    {
+      // Con el LOCALE_ID establece este idioma, ya que por defecto esta en ingles
+      provide: LOCALE_ID, useValue: 'es-MX'
+    }
   ],
   bootstrap: [AppComponent]
 })
