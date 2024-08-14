6d7cffaf937ebeede17cf36fbed8a1ed8c80307e
Angel <angelsistemas21@gmail.com>
2024-08-14 16:21:35 -0600

    chore: deploy build to GitHub Pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WVFGEIG4.mjs";import"./chunk-E3PPES5Z.mjs";import"./chunk-NGHAEBZM.mjs";import"./chunk-GAURCFXL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
