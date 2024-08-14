7d213cd49a1426f1437a0d0a7b748c2a13bf941e
Angel <angelsistemas21@gmail.com>
2024-08-14 10:36:10 -0600

    feat: add structure HTML and logic in the order component

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from './interfaces/hero.interfaces';
 
 @Component({
   selector: 'products-order',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class OrderComponent {
+
+  public isUpperCase: boolean = false;
+  public valueOrder?: keyof Hero;
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue,
+    },
+    {
+      name: 'Green Lanter',
+      canFly: true,
+      color: Color.green,
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black,
+    },
+    {
+      name: 'Flash',
+      canFly: false,
+      color: Color.red,
+    },
+    {
+      name: 'Iron man',
+      canFly: true,
+      color: Color.red,
+    },
+    {
+      name: 'Aquaman',
+      canFly: false,
+      color: Color.blue,
+    },
+    {
+      name: 'X-men',
+      canFly: false,
+      color: Color.black,
+    },
+  ]
+
+  toggleUpperCase(): void{
+    this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: keyof Hero): void{
+    this.valueOrder = value;
+  }
 
 }
